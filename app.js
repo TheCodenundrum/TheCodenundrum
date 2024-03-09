be620f9f5b0f3f1bfd4ce31f813436fc33244b8d
MitchellPhelps <MitchellRPhelps99@gmail.com>
2024-03-09 13:43:17 -0700

    Adding Bootstrap.

--- app.js
+++ app.js
@@ -1,28 +1,48 @@
+/*
+app.js
+
+TheCodenundrum app runner.
+*/
+
+// ----------------------------------------------------------------------------
+// Requires
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// ----------------------------------------------------------------------------
+// Router Requires
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+// ----------------------------------------------------------------------------
+// Instantiate App
 var app = express();
 
-// view engine setup
+// ----------------------------------------------------------------------------
+// View Engine Setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// ----------------------------------------------------------------------------
+// Libraries
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
 
+// ----------------------------------------------------------------------------
+// Routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
+// ----------------------------------------------------------------------------
+// 404
 app.use(function(req, res, next) {
   next(createError(404));
 });
